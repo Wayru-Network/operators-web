8067d13f44b51c0c530ad2989f5b1fdcc7a7ad7c
esfxra <diegoserranor@gmail.com>
2025-06-30 12:53:08 -0500

    update hero components to beta

---
+++ app/[lang]/(operator)/captive-portal/_services/get-captive-portals.ts
@@ -0,0 +1,16 @@
+export default async function getCaptivePortals() {
+  return [
+    {
+      id: "1",
+      name: "Wayru",
+      description: "Wayru is a captive portal for Wayru",
+      url: "/wayru",
+    },
+    {
+      id: "2",
+      name: "Wayru",
+      description: "Wayru is a captive portal for Wayru",
+      url: "/wayru",
+    },
+  ];
+}
