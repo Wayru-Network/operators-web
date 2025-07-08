87bd3f9dc732a969c3cc198b583bf03626122a6f
danvelc <danvelc6@gmail.com>
2025-07-08 12:57:22 -0500

    Montserrat font setup

--- lib/ui/font.ts
+++ lib/ui/font.ts
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Montserrat } from "next/font/google";
 
 export const GeistSans = Geist({
   variable: "--font-geist-sans",
@@ -7,5 +7,10 @@ export const GeistSans = Geist({
 
 export const GeistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const MontserratFont = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
 });
