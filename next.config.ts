cad58da2f8d1d1b6e2a85fd97c6a232b4109a927
danvelc <danvelc6@gmail.com>
2025-07-17 19:56:17 -0500

    obtaining blob assets URL via SAS token auth

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,23 @@
 import type { NextConfig } from "next";
 
+const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME!;
+
 const nextConfig: NextConfig = {
   output: "standalone",
   experimental: {
     serverActions: {
       bodySizeLimit: "40mb",
     },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: `${accountName}.blob.core.windows.net`,
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
