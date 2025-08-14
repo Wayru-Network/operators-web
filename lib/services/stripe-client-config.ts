46e8de8662bb72a37744e49019ed4c8bfb5aaade
slipiduche <alejandrocamacaro91@gmail.com>
2025-08-14 09:52:03 -0400

    updating env import

--- lib/services/stripe-client-config.ts
+++ lib/services/stripe-client-config.ts
@@ -1,4 +1,5 @@
 "use client"
 import { loadStripe } from '@stripe/stripe-js';
+import { env } from '@/lib/infra/env';
 
-export const stripeClient = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+export const stripeClient = loadStripe(env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
