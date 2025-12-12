a4b71f08153cc29e5b79bd97297dbbc86bc78313
danvelc <81994050+danvelc@users.noreply.github.com>
2025-12-12 13:48:36 -0500

    subscription service response and documentation

--- lib/services/subscription-service.ts
+++ lib/services/subscription-service.ts
@@ -1,7 +1,21 @@
 "use server";
 import { Prisma } from "@/lib/infra/prisma";
+import { getSession } from "@/lib/session/session";
 
-export async function getSubscriptionStatus(userId: string): Promise<boolean> {
+export interface SubscriptionStatusResponse {
+  able: boolean;
+  maxPortals: number;
+}
+
+// Function to check if the user has a valid subscription
+// This function can be used in the subscription service to determine if a user has an active subscription
+export async function getSubscriptionStatus(): Promise<boolean> {
+  const session = await getSession();
+  if (!session?.userId) {
+    return false;
+  }
+  const userId = session.userId;
+
   const customer = await Prisma.customers.findFirst({
     where: {
       customer_uuid: userId,
