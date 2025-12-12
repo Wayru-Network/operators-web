78abc1ff84682f3793d76be0edff8945e3fee7d1
danvelc <81994050+danvelc@users.noreply.github.com>
2025-12-12 13:49:07 -0500

    CreateCaptivePortal component validSub implementation

--- app/[lang]/(operator)/captive-portal/new-portal/page.tsx
+++ app/[lang]/(operator)/captive-portal/new-portal/page.tsx
@@ -2,6 +2,7 @@ import CreateCaptivePortal from "./_components/create-captive-portal";
 import { getAllHotspots } from "../../hotspots/_services/get-hotspots";
 import { canCreatePortal } from "./_services/can-create-portal";
 import { redirect } from "next/navigation";
+import { getSubscriptionStatus } from "@/lib/services/subscription-service";
 
 export default async function CaptivePortal() {
   //Prevent access if user has no valid subscription and already has 1 portal
@@ -9,8 +10,11 @@ export default async function CaptivePortal() {
   if (!canCreate.able) {
     redirect("/captive-portal");
   }
+
+  // check if the user has a valid subscription
+  const validSub = await getSubscriptionStatus();
   // Fetch portals data from the server
   const hotspots = await getAllHotspots();
   // const hotspots = await getHotspotsToAssignCaptivePortal();
-  return <CreateCaptivePortal hotspots={hotspots} />;
+  return <CreateCaptivePortal hotspots={hotspots} subStatus={validSub} />;
 }
